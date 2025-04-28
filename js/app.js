6f69c6e8ac6bd5823dd3c318d681a081b0c81585
piash-ahmed <aryanpiash420@gmail.com>
2025-04-28 13:07:29 +0600

    adopt pet section's dynamic categori buttons added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('connected');
+// Load Categories data for Category Buttons
+const loadCategories = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/categories`)
+    const data = await res.json()
+
+    const categoriesContainer = document.getElementById('categories-container')
+    data.categories.forEach((category) => {
+        const categoryBtn = document.createElement('div')
+        categoryBtn.innerHTML = `
+        <button class="btn px-10 py-7 lg:px-16 lg:py-8 rounded-lg text-lg lg:text-xl font-bold border-[#cfe4e6] hover:bg-[#0e79813b]"><img class="w-10 mr-2" src=${category.category_icon}> ${category.category}</button>
+        `
+        categoriesContainer.append(categoryBtn)
+    })
+}
+loadCategories()
