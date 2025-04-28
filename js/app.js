78d514e232ab73933fd667ecd9d9cb10e23c4bff
piash-ahmed <aryanpiash420@gmail.com>
2025-04-28 22:39:50 +0600

    get all pets info from Api and show as card in all pets section

--- js/app.js
+++ js/app.js
@@ -13,3 +13,50 @@ const loadCategories = async () => {
     })
 }
 loadCategories()
+
+// Load all pets and output in card
+const loadAllPets = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pets`)
+    const data = await res.json()
+
+    const allPetsContainer = document.getElementById('pets-container')
+
+    data.pets.forEach((pet) => {
+        const petInfoCard = document.createElement('div')
+
+
+        petInfoCard.innerHTML = `
+        <div class="card border border-gray-200 p-4 md:p-4">
+            <figure class="h-[160px]"><img class="h-full w-full object-cover rounded-lg" src=${pet.image} alt="Shoes" /></figure>
+            <div class="py-4 pl-1">
+            <h2 class="card-title text-lg font-extrabold mb-2">${pet.pet_name}</h2>
+            
+            <p class="flex items-center gap-1 text-gray-700 text-sm mb-1"><img class="w-4 h-4" src="assets/icons/breed.png" >
+            Breed: ${pet.breed ? `${pet.breed}` : `Not Available`}</p>
+
+            <p class="flex items-center gap-1 text-gray-700 text-sm mb-1"><img class="w-4 h-4" src="assets/icons/birth.png" >
+            Birth: ${pet.date_of_birth ? `${pet.date_of_birth}` : `Not Available`}</p>
+
+            <p class="flex items-center gap-1 text-gray-700 text-sm mb-1"><img class="w-4 h-4" src="assets/icons/gender.png" >
+            Gender: ${pet.gender ? `${pet.gender}` : `Not Available`}</p>
+
+            <p class="flex items-center gap-1 text-gray-700 text-sm"><img class="w-4 h-4" src="assets/icons/price.png" >
+            Price: ${pet.price ? `${pet.price}$` : `Not Available`}</p>
+            <div class="divider opacity-65"></div>
+            <div class="flex items-center justify-center gap-2">
+            <button class="btn px-5 hover:bg-[#0e79813b] hover:border-transparent"><img class="h-5 w-5" src="assets/icons/like.png" ></button>
+            <button class="btn px-5 hover:bg-[#0e79813b] hover:border-transparent">Adopt</button>
+            <button class="btn px-5 hover:bg-[#0e79813b] hover:border-transparent">Detail</button>
+            </div>
+
+            </div>
+        </div>
+        `
+
+        allPetsContainer.appendChild(petInfoCard)
+    })
+
+
+
+}
+loadAllPets()
