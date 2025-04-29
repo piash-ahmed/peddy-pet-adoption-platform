5ee6221c1c91639f019f0a0351172ce6ecf4b754
piash-ahmed <aryanpiash420@gmail.com>
2025-04-29 22:12:31 +0600

    added dynamic custom modal for detail button

--- js/app.js
+++ js/app.js
@@ -29,11 +29,11 @@ const displayPets = (data) => {
     data.forEach((pet) => {
         const petInfoCard = document.createElement('div')
         // console.log(pet);
-        
+
         petInfoCard.innerHTML = `
         <div class="card border border-gray-200 p-4 md:p-4">
             <figure class="h-[160px]"><img class="h-full w-full object-cover rounded-lg" src=${pet.image} alt="Shoes" /></figure>
-            <div class="py-4 pl-1">
+            <div class="pt-4 pl-1">
             <h2 class="card-title text-lg font-extrabold mb-2">${pet.pet_name}</h2>
             
             <p class="flex items-center gap-1 text-gray-700 text-sm mb-1"><img class="w-4 h-4" src="assets/icons/breed.png" >
@@ -51,7 +51,7 @@ const displayPets = (data) => {
             <div class="flex items-center justify-center gap-2">
             <button class="btn px-5 hover:bg-[#0e79813b] hover:border-transparent"><img class="h-5 w-5" src="assets/icons/like.png" ></button>
             <button id=${pet.petId} onclick="addToAdopt('${pet.image}', ${pet.petId})" class="btn px-5 hover:bg-[#0e79813b] hover:border-transparent">Adopt</button>
-            <button class="btn px-5 hover:bg-[#0e79813b] hover:border-transparent">Detail</button>
+            <button onclick="detailsModal(${pet.petId})" class="btn px-5 hover:bg-[#0e79813b] hover:border-transparent">Detail</button>
             </div>
             </div>
         </div>
@@ -62,7 +62,6 @@ const displayPets = (data) => {
 
 // Add to Adopt section
 const addToAdopt = (image, petId) => {
-    console.log(image);
     const adoptedPetSection = document.getElementById('adopted-pets')
     const adoptedPet = document.createElement('img')
     adoptedPet.classList = 'w-30 inline-block rounded-lg'
@@ -73,7 +72,9 @@ const addToAdopt = (image, petId) => {
     const adoptBtn = document.getElementById(petId)
     adoptBtn.disabled = true
 }
-const loadPetsByCategory = async(category) => {
+
+// Load pets by category dynamic query
+const loadPetsByCategory = async (category) => {
     const url = `https://openapi.programming-hero.com/api/peddy/category/${category}`
     const res = await fetch(url)
     const data = await res.json()
@@ -81,3 +82,39 @@ const loadPetsByCategory = async(category) => {
     displayPets(data.data)
 }
 
+// Show Details Modal when Detail button clicked
+const detailsModal = async (petId) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${petId}`)
+    const data = await res.json()
+    const petDetail = data.petData
+
+    const modalContent = document.getElementById('modal-content')
+    modalContent.innerHTML = `
+    <img class="w-full rounded-lg" src=${petDetail.image}/>
+     <h2 class="card-title text-xl font-extrabold mb-3 mt-5">${petDetail.pet_name}</h2>
+     <div class="flex gap-x-6 md:gap-x-10">
+        <div>
+            <p class="flex items-center gap-1 text-gray-700 text-sm mb-1"><img class="w-4 h-4" src="assets/icons/breed.png" >
+            Breed: ${petDetail.breed ? `${petDetail.breed}` : `Not Available`}</p>
+
+            <p class="flex items-center gap-1 text-gray-700 text-sm mb-1"><img class="w-4 h-4" src="assets/icons/gender.png" >
+            Gender: ${petDetail.gender ? `${petDetail.gender}` : `Not Available`}</p>
+
+            <p class="flex items-center gap-1 text-gray-700 text-sm mb-1"><img class="w-4 h-4" src="assets/icons/gender.png" >
+            Vaccinated status: ${petDetail.vaccinated_status ? `${petDetail.vaccinated_status}` : `Not Available`}</p>
+        </div>
+        <div>
+            <p class="flex items-center gap-1 text-gray-700 text-sm mb-1"><img class="w-4 h-4" src="assets/icons/price.png" >
+            Price: ${petDetail.price ? `${petDetail.price}$` : `Not Available`}</p>
+
+            <p class="flex items-center gap-1 text-gray-700 text-sm mb-1"><img class="w-4 h-4" src="assets/icons/birth.png" >
+            Birth: ${petDetail.date_of_birth ? `${petDetail.date_of_birth}` : `Not Available`}</p>
+        </div>
+        </div>
+        <div class="divider opacity-65 my-1"></div>
+        <h3 class="font-bold mb-2 mt-4">Details Information</h3>
+        <p class="text-sm text-gray-700 mb-4">${petDetail.pet_details}</p>
+    `
+    const customModal = document.getElementById('customModal')
+    customModal.showModal()
+}
